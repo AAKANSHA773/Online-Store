00490ca8642fdce59e41a0ac5132c8e504e97fd4
AAKANSHA773 <aakanshapanwar01@gmail.com>
2025-12-31 12:28:56 +0530

    Create Online Store App and setup the basic requirment and Tailwind Css

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+     <h1 className="text-3xl font-bold underline">
+     ProductList
+    </h1>
+  )
+}
+
+export default ProductList
